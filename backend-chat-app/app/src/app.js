b57f5893bccfc73a343490113bcb01c8ddeeee6f
Maciej Rybarczyk <34245941+M-Ryba@users.noreply.github.com>
2025-06-04 00:23:41 +0200

    chore: change env variable name

--- backend-chat-app/app/src/app.js
+++ backend-chat-app/app/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
   })
