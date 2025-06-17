ee7730f2fdd91c5c3d55cf0fb81dcb4777968a02
Maciej Rybarczyk <34245941+M-Ryba@users.noreply.github.com>
2025-06-17 22:30:01 +0200

    fix: further problems with containers

--- frontend-chat-app/app/vite.config.js
+++ frontend-chat-app/app/vite.config.js
@@ -12,7 +12,12 @@ export default defineConfig({
 			outdir: './src/lib/paraglide'
 		})
 	],
+	server: {
+		port: '5173',
+		host: '0.0.0.0'
+	},
 	preview: {
+		port: '5173',
 		host: '0.0.0.0'
 	}
 });
